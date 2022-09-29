7390b78103479524974d1ef29d27d68c641937ad
Mariana Antúnez <104743384+Antz06@users.noreply.github.com>
2022-09-28 22:50:48 -0500

    Add files via upload

---
+++ Permutacion2.py
@@ -0,0 +1,34 @@
+class Permutaciones:
+    # Fórmula matemática de las permutaciones: rPn = n! / (n - r)!
+    __lista = []
+    __r = 0
+    __resultadoPermutacion = 0
+
+    def permutacion(self):
+        self.__lista = {1, 2, 5, 6, 7, 8}
+        self.__r = 6
+        p = Permutaciones()
+        n = p.factorial(len(self.__lista))
+
+        if (self.__r >= len(self.__lista)):
+            r = 1
+        else:
+            r = p.factorial(len(self.__lista) - self.__r)
+        self.__resultadoPermutacion = n / r
+
+        print(self.__resultadoPermutacion)
+        return self.__resultadoPermutacion
+
+    def factorial(self, n):
+
+        if (n == 0):
+            return 1
+        else:
+            return self.factorial(n - 1) * n
+
+def main():
+    ejecutar = Permutaciones()
+    ejecutar.permutacion()
+
+if __name__ == '__main__':
+    main()
